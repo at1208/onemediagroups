24717a2b1a622d41167c31ec6a4a80188d8d3a56
at1208 <amantiwari12081999@gmail.com>
2021-07-04 03:20:57 +0530

    fix: design improved

--- client/src/components/notification/list.js
+++ client/src/components/notification/list.js
@@ -5,9 +5,11 @@ import { getCookie } from '../../actions/auth';
 import moment from 'moment';
 import {
  ListItem,
- ListItemText,
  Typography,
- Grid
+ Card,
+ Grid,
+ Box,
+ TextField
 } from "@material-ui/core";
 
 const NotificationList = ({ viewAs }) => {
@@ -29,20 +31,39 @@ const NotificationList = ({ viewAs }) => {
 const list = () => {
   if(notifyList.length>0){
   return notifyList.map((msg, i) => {
-      let desc = JSON.parse(msg.description)
+      let desc = JSON.parse(msg.description);
       return <>
            <ListItem>
-              <ListItemText
-                primary={msg.title}
-                primaryTypographyProps={{
-                  variant: "subtitle2",
-                  color: "textPrimary",
-                }}
-                secondary={desc.description}
-              />
+              <Card>
+              <Box p={1} position="relative">
+             <Grid container>
+               <Grid item xs={12} sm={12} md={12} lg={12}>
+               <Typography variant="h6"   style={{ fontWeight:600 }}>
+                 {msg.title}
+               </Typography>
+               </Grid>
+               <Grid item xs={12} sm={12} md={12} lg={12}>
+               <TextField
+                  fullWidth
+                  disabled
+                  multiline
+
+                  size="small"
+                  value={desc.description} />
+               </Grid>
+             </Grid>
+              <Box position="absolute" bottom={0} right={10}>
               <Typography variant="body2">
                <b>{moment(msg.createdAt).format(" h:mm a, Do MMMM YYYY")}</b>
               </Typography>
+              </Box>
+             </Box>
+          </Card>
+
+
+
+
+              {/**/}
            </ListItem>
 
            </>
