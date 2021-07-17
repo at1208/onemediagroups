c8dc6fae93c71386cc59c0e4ae10ea59afaa1b27
at1208 <amantiwari12081999@gmail.com>
2021-07-17 11:03:25 +0530

    bundle optimized

--- server/models/blog_model.js
+++ server/models/blog_model.js
@@ -1,70 +1,76 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
-const blogSchema = mongoose.Schema({
-      title: {
-        type: String,
-        trim: true,
-        min: 3,
-        max: 400,
-        required: true
-      },
-      slug: {
-        type: String,
-        unique: true,
-        index: true
-      },
-      body: {
-        type: {},
-        required: true,
-        min: 200,
-        max: 2000000
-      },
-      excerpt: {
-        type: String,
-        max: 1000
-      },
-      mtitle: {
-        type: String
-      },
-      mdesc: {
-        type: String
-      },
-      featureImg:{
-        type:String,
-        required:true
-      },
-      categories: [{ type: ObjectId, ref: 'Category', required: true }],
-      postedBy: {
-        type: ObjectId,
-        ref: 'Employee'
-      },
-      domain: {
-        type: ObjectId,
-        ref: 'Domain'
-      },
-      status: {
-        type: Boolean,
-        default: false
-      },
-      approval:{
-        type:String,
-        enum:["WAITING", "APPROVED", "NOT APPROVED"],
-        default:"WAITING"
-      },
-      views_count:{
-        type:Number,
-        default:0
-      },
-      updatedBy:{
-        type:ObjectId,
-        ref:"Employee"
-      },
-      task:{
-        type:ObjectId,
-        ref:"Task",
-        unique:true,
-        required:true
-      }
-}, { timestamps: true })
+const blogSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 400,
+      required: true
+    },
+    slug: {
+      type: String,
+      unique: true,
+      index: true
+    },
+    body: {
+      type: {},
+      required: true,
+      min: 200,
+      max: 2000000
+    },
+    excerpt: {
+      type: String,
+      max: 1000,
+      trim: true
+    },
+    mtitle: {
+      type: String,
+      trim: true
+    },
+    mdesc: {
+      type: String,
+      trim: true
+    },
+    featureImg: {
+      type: String,
+      required: true
+    },
+    categories: [{ type: ObjectId, ref: "Category", required: true }],
+    postedBy: {
+      type: ObjectId,
+      ref: "Employee"
+    },
+    domain: {
+      type: ObjectId,
+      ref: "Domain"
+    },
+    status: {
+      type: Boolean,
+      default: false
+    },
+    approval: {
+      type: String,
+      enum: ["WAITING", "APPROVED", "NOT APPROVED"],
+      default: "WAITING"
+    },
+    views_count: {
+      type: Number,
+      default: 0
+    },
+    updatedBy: {
+      type: ObjectId,
+      ref: "Employee"
+    },
+    task: {
+      type: ObjectId,
+      ref: "Task",
+      unique: true,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model("Blog", blogSchema);
