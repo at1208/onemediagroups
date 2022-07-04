c393838e972fc4346e88cf75701cf9d5b0c968b4
at1208 <amantiwari12081999@gmail.com>
2022-07-04 12:31:41 +0530

    feat: APIs added

--- server/controllers/blog_controller.js
+++ server/controllers/blog_controller.js
@@ -391,7 +391,7 @@ module.exports.blog_by_category = (req, res) => {
 
     Blog.find({
       domain: { $eq: domainId },
-      categories: { $in: result?._id },
+      categories: { $in: result._id },
       approval: { $in: "APPROVED" },
       status: { $in: true },
     })
